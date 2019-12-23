e2d1f10d8e0ae238550c9a07b4c560fdca3e46f6
RSchneider94 <rodrigoschneider94@gmail.com>
2019-12-23 14:57:53 +0100

    Implemented form to enter player names

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import FormStartGame from "./components/FormStartGame";
 import GameInfoBar from "./components/GameInfoBar";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  if (gameStarted) {
+    return (
+      <div>
+        <GameInfoBar></GameInfoBar>
+        <GameBoard></GameBoard>
+      </div>
+    );
+  }
   return (
     <div>
-      <GameInfoBar></GameInfoBar>
-      <GameBoard></GameBoard>
+      <FormStartGame></FormStartGame>
     </div>
   );
 }
---
+++ src/components/FormStartGame.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledFormStartGame = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 80px auto 0;
+  padding-bottom: 40px;
+  width: 80%;
+  background-color: #fff;
+  border-radius: 2%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+`;
+
+const StyledWelcomeText = styled.h2`
+  height: fit-content;
+  text-align: center;
+  font-size: 2.5em;
+  padding-left: 2em;
+  border-bottom: 2px solid #5d3455;
+`;
+
+const StyledFormGroup = styled.div`
+  margin: 20px 0;
+`;
+
+const StyledInputText = styled.input`
+  width: 40%;
+  display: inline-block;
+  padding: 6px;
+  border: 1px solid #5d3455;
+  border-radius: 2px;
+
+  &:focus {
+    border: 2px solid #5d3455;
+    outline: 0;
+  }
+`;
+
+const StyledSubmitButton = styled.button`
+  background-color: #5d3455;
+  border: 1px solid #5d3455;
+  color: #fff;
+  padding: 10px;
+  border-radius: 2px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log("form submitted");
+};
+
+function FormStartGame() {
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+
+  return (
+    <StyledFormStartGame>
+      <StyledWelcomeText>
+        Welcome! Let's start?{" "}
+        <span role="img" aria-label="smile">
+          😀
+        </span>
+      </StyledWelcomeText>
+      <form
+        onSubmit={handleSubmit}
+        style={{ width: "100%", textAlign: "center" }}
+      >
+        <StyledFormGroup>
+          <label style={{ display: "block", margin: "10px 0" }}>
+            Player 1:
+          </label>
+          <StyledInputText
+            type="text"
+            name="player1"
+            id="player1"
+            placeholder="Enter the name of player 1"
+            value={player1}
+            onChange={e => setPlayer1(e.target.value)}
+          ></StyledInputText>
+        </StyledFormGroup>
+        <StyledFormGroup>
+          <label style={{ display: "block", margin: "10px 0" }}>
+            Player 2:
+          </label>
+          <StyledInputText
+            type="text"
+            name="player2"
+            id="player2"
+            placeholder="Enter the name of player 2"
+            value={player2}
+            onChange={e => setPlayer2(e.target.value)}
+          ></StyledInputText>
+        </StyledFormGroup>
+        <StyledFormGroup>
+          <StyledSubmitButton type="submit">Start the Game!</StyledSubmitButton>
+        </StyledFormGroup>
+      </form>
+    </StyledFormStartGame>
+  );
+}
+
+export default FormStartGame;
